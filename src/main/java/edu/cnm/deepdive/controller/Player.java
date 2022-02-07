b06c03bde2d0aedd10d3c79d9f8a6dd3d8b6e0c4
Kyle Coleman <kcoleman20@cnm.edu>
2022-02-07 12:05:17 -0700

    Implement a validation with internationalize messages

--- src/main/java/edu/cnm/deepdive/controller/Player.java
+++ src/main/java/edu/cnm/deepdive/controller/Player.java
@@ -13,11 +13,15 @@ public class Player {
   private static final String GUESS_PROMPT_FORMAT_KEY = "guess_prompt_format";
   private static final String GUESS_OUTCOME_FORMAT_KEY = "guess_outcome_format";
   private static final String SUMMARY_FORMAT_KEY = "summary_format";
+  public static final String INVALID_LENGTH_FORMAT = "invalid_length_format";
+  public static final String INVALID_CHARS_FORMAT_KEY = "invalid_chars_format";
+  public static final String GUESS_VALIDATION_FORMAT = "^[%1$s]{%2$d}$";
 
   private final GameRepository repository;
   private final Scanner scanner;
   private final PrintStream output;
   private final ResourceBundle bundle;
+
 
   public Player(Scanner scanner, PrintStream output,
       ResourceBundle bundle) {
@@ -30,8 +34,9 @@ public class Player {
   public void play(String pool, int length) throws IOException {
     Game game = repository.startGame(pool, length);
     Guess guess;
+    String validGuessPattern = String.format(GUESS_VALIDATION_FORMAT, game.getPool(), game.getLength());
     do {
-      String input = getGuess(game);
+      String input = getGuess(game, validGuessPattern);
       guess = repository.submitGuess(game, input);
       displayOutcome(guess);
     } while (!game.isSolved());
@@ -39,13 +44,26 @@ public class Player {
     // TODO: Explore starting time from first guess
     long elapsedTime = endTime - game.getCreated().getTime();
     long seconds = Math.round(elapsedTime / 1000.0);
-    System.out.printf(bundle.getString(SUMMARY_FORMAT_KEY), game.getText(), game.getGuesses().size(), seconds);
+    System.out.printf(bundle.getString(SUMMARY_FORMAT_KEY), game.getText(),
+        game.getGuesses().size(), seconds);
   }
 
-  private String getGuess(Game game) {
+  private String getGuess(Game game, String validationPattern) {
     output.printf(bundle.getString(GUESS_PROMPT_FORMAT_KEY), game.getPool(), game.getLength());
-    String input = scanner.nextLine().trim();
-    //TODO: Validate input using the pool and length.
+    String input;
+    do {
+      input = scanner.nextLine().trim();
+      //TODO: Validate input using the pool and length.
+      if (input.length() != game.getLength()) {
+        System.out.printf(bundle.getString(INVALID_LENGTH_FORMAT), input,
+            game.getLength());
+      } else if (!input.matches(validationPattern)) {
+        System.out.printf(bundle.getString(INVALID_CHARS_FORMAT_KEY),
+            input, game.getPool());
+      } else {
+        break;
+      }
+    } while (true);
     return input;
   }
 
